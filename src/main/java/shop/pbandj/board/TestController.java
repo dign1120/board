53789fe4e077ec23593823c604d46c4f501717cd
dign1120 <dign1120@naver.com>
2023-11-27 20:12:14 +0900

    초기 설정 (3)

--- src/main/java/shop/pbandj/board/TestController.java
+++ src/main/java/shop/pbandj/board/TestController.java
@@ -3,7 +3,6 @@ package shop.pbandj.board;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Configuration
 @RestController
 public class TestController {
     @GetMapping("")
